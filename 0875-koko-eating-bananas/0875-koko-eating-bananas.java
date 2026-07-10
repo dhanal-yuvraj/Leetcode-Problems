ef24903daf5d10544b833ce17cd966b149539444
Yuvraj dhanal <dhanal.yuvraj06@gmail.com>
2026-07-10 15:27:46 +0530

    Time: 26 ms (16.82%), Space: 48.4 MB (10.95%) - LeetHub

--- 0875-koko-eating-bananas/0875-koko-eating-bananas.java
+++ 0875-koko-eating-bananas/0875-koko-eating-bananas.java
@@ -1,38 +1,31 @@
 class Solution {
-    public int findMax(int[] arr) {
+    public int maxElement(int[] arr){
         int maximum = Integer.MIN_VALUE;
-        for (int i = 0; i < arr.length; i++) {
+        for(int i=0; i<arr.length;i++){
             maximum = Math.max(maximum, arr[i]);
         }
         return maximum;
     }
-
-    public long hours(int[] piles, int hour) {
-        long totalHrs = 0;
-
-        for (int i = 0; i < piles.length; i++) {
-            totalHrs += (piles[i] + (long)hour - 1) / hour;
+    public int HourEating(int piles[], int hour){
+        int totalSum = 0;
+        for(int i=0; i<piles.length;i++){
+            totalSum += Math.ceil((double)piles[i]/hour);
         }
-
-        return totalHrs;
+        return totalSum;
     }
-
     public int minEatingSpeed(int[] piles, int h) {
         int left = 1;
-        int right = findMax(piles);
-        int ans = right;
-
-        while (left <= right) {
-            int mid = left + (right - left) / 2;
-
-            if (hours(piles, mid) <= h) {
-                ans = mid;
-                right = mid - 1;
-            } else {
-                left = mid + 1;
+        int right = maxElement(piles);
+        int res = right;
+        while(left<=right){
+            int mid = (left+right)/2;
+            int totalHrs = HourEating(piles, mid);
+            if(totalHrs<=h){
+                right = mid-1;
+            }else{
+                left = mid+1;
             }
         }
-
-        return ans;
+        return left;
     }
 }
