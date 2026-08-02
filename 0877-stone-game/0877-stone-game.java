254ade78d7ab18fb5b91631fad63b7a2ef63b817
Yuvraj dhanal <dhanal.yuvraj06@gmail.com>
2026-08-02 11:14:54 +0530

    Time: 0 ms (100%), Space: 43.1 MB (62.03%) - LeetHub

---
+++ 0877-stone-game/0877-stone-game.java
@@ -0,0 +1,12 @@
+class Solution {
+    public boolean stoneGame(int[] piles) {
+        // int odd = 0;
+        // int even = 0;
+        // for(int i=0; i<piles.length;i++){
+        //     if(n%i==0){
+        //         even += 
+        //     }
+        // }
+        return true;
+    }
+}
