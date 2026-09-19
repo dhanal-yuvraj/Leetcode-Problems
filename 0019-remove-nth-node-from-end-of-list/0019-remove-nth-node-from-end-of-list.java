3542fc9f516ce4d6c0765e5f954a2bc3b4c506f2
Yuvraj dhanal <dhanal.yuvraj06@gmail.com>
2026-09-19 18:57:46 +0530

    Time: 0 ms (100%), Space: 43.1 MB (94.56%) - LeetHub

--- 0019-remove-nth-node-from-end-of-list/0019-remove-nth-node-from-end-of-list.java
+++ 0019-remove-nth-node-from-end-of-list/0019-remove-nth-node-from-end-of-list.java
@@ -9,41 +9,27 @@
  * }
  */
 class Solution {
-    public ListNode reverse(ListNode head){
-        if(head==null || head.next==null){
-            return head;
-        }
-        ListNode newHead = reverse(head.next);
-        ListNode front = head.next;
-        front.next = head;
-        head.next = null;
-        return newHead;
-    }
     public ListNode removeNthFromEnd(ListNode head, int n) {
         if(head==null || head.next==null){
             return null;
         }
-        ListNode revHead = reverse(head);
-        if(n==1){
-            revHead = revHead.next;
-            return reverse(revHead);
+        ListNode fast = head;
+        ListNode slow = head;
+        for(int i=0; i<n;i++){
+            fast = fast.next;
+        }
+        if(fast == null){
+            head = head.next;
+            return head;
+        }
+        while(fast.next != null){
+            fast = fast.next;
+            slow = slow.next;
         }
 
-        ListNode prev = revHead;
-        int count = 0;
-        ListNode temp = revHead;
-        while(temp!=null){
-            count++;
-            if(count==n){
-                break;
-            }
-            prev = temp;
-            temp = temp.next;
-        }
-
-        ListNode front = temp.next;
-        prev.next = front;
-        temp.next = null;
-        return reverse(revHead);
+        ListNode deleteNode = slow.next;
+        slow.next = slow.next.next;
+        deleteNode.next = null;
+        return head;
     }
 }
